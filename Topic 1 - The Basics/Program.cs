219610c0bb5325989756ffe4e11a9b78c3c5e69b
Nathan Barnes <339723215@GoTVDSB.ca>
2024-09-10 11:55:05 -0400

    just started

--- Topic 1 - The Basics/Program.cs
+++ Topic 1 - The Basics/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace Topic_1___The_Basics
+﻿using System.Net.Security;
+
+namespace Topic_1___The_Basics
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            // Nate
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("i will print on a new line");
+            Console.WriteLine();
+            Console.Write("Hello worlkd");
+            Console.Write("print in same line");
+         
         }
+            
+
+
+
+        
+
+
+
     }
 }
